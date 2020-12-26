fef0952dd8bcd7ac006d390c048129391d965f47
CosmoBoyMe <2014failz2014@gmail.com>
2020-12-26 17:06:07 +0100

    add new function difference two file

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import commander from 'commander';
+import getDiff from './index.js';
 
 const gendiff = commander;
 
@@ -7,7 +8,11 @@ gendiff
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.0', '-V, -v, --version', 'output the version number')
   .option('-F, -f, --format [type]', 'output format', 'stylish')
-  .helpOption('-H, -h, --help', 'output usage information');
+  .helpOption('-H, -h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    const fileDifference = getDiff(filepath1, filepath2);
+    console.log(fileDifference);
+  });
 
 const runCli = () => {
   gendiff.parse(gendiff.argv);
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const getDiff = (filepath1, filepath2) => {
+  const data1 = JSON.parse(fs.readFileSync(filepath1));
+  const data2 = JSON.parse(fs.readFileSync(filepath2));
+  const keys = _.union(_.keys(data1), _.keys(data2));
+  const keysSorted = keys.sort();
+
+  const differences = keysSorted.reduce((acc, key) => {
+    let currentDifferences = acc;
+
+    const addedKey = `\n  + ${key}: ${_.get(data2, key)}`;
+    const oldChangedKey = `\n  - ${key}: ${_.get(data1, key)}`;
+    const newChangedKey = `\n  + ${key}: ${_.get(data2, key)}`;
+    const deletedKey = `\n  - ${key}: ${_.get(data1, key)}`;
+    const unchangedKey = `\n    ${key}: ${_.get(data1, key)}`;
+
+    if (!_.has(data1, key)) {
+      currentDifferences += addedKey;
+    } else if (!_.has(data2, key)) {
+      currentDifferences += deletedKey;
+    } else if (data1[key] !== data2[key]) {
+      currentDifferences += oldChangedKey;
+      currentDifferences += newChangedKey;
+    } else {
+      currentDifferences += unchangedKey;
+    }
+    return currentDifferences;
+  }, '');
+
+  return `\n{${differences}\n}`;
+};
+
+export default getDiff;
