c3316690b9a7235ee17aa6ad04efe0f096d57f68
CosmoBoyMe <2014failz2014@gmail.com>
2021-01-17 22:44:04 +0100

    add yaml format

---
+++ src/parsers.js
@@ -0,0 +1,17 @@
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
+
+const parsers = (filePath) => {
+  const format = path.extname(filePath);
+  switch (format) {
+    case '.yaml':
+      return yaml.load(fs.readFileSync(filePath, 'utf8'));
+    case '.json':
+      return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    default:
+      throw new Error('Error format');
+  }
+};
+
+export default parsers;
