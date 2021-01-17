5cdc199233be0f82ede8489450716ff2355e8aa0
CosmoBoyMe <2014failz2014@gmail.com>
2021-01-17 22:41:34 +0100

    add default format

--- src/cli.js
+++ src/cli.js
@@ -6,11 +6,11 @@ const gendiff = commander;
 gendiff
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.0', '-V, -v, --version', 'output the version number')
-  .option('-F, -f, --format [type]', 'output format', 'stylish')
-  .helpOption('-H, -h, --help', 'output usage information')
+  .version('0.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
-    const fileDifference = getDiff(filepath1, filepath2);
+    const fileDifference = getDiff(filepath1, filepath2, gendiff.opts().format);
     console.log(fileDifference);
   });
 
