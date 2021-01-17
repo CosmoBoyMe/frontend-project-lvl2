f2252047980ce83e01acb1999669a4179be9359b
CosmoBoyMe <2014failz2014@gmail.com>
2021-01-17 22:42:14 +0100

    add new logic

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,34 +1,34 @@
-import fs from 'fs';
 import _ from 'lodash';
+import parsers from './parsers.js';
+import getStylishFormat from './formaters/stylish.js';
+
+const getSyntaxTree = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2));
+
+  const keysSorted = keys.sort();
+  const ast = keysSorted.map((key) => {
+    if (!_.has(data1, key)) {
+      return { name: key, value: data2[key], status: 'added' };
+    } if (!_.has(data2, key)) {
+      return { name: key, value: data1[key], status: 'deleted' };
+    } if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      const children = getSyntaxTree(data1[key], data2[key]);
+      return { name: key, status: 'nested', children };
+    } if (data1[key] !== data2[key]) {
+      return {
+        name: key, value: data2[key], oldValue: data1[key], status: 'changed',
+      };
+    }
+    return { name: key, value: data1[key], status: 'unchanged' };
+  });
+  return ast;
+};
 
 const getDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(fs.readFileSync(filepath1, 'utf8'));
-  const data2 = JSON.parse(fs.readFileSync(filepath2, 'utf8'));
-  const keys = _.union(_.keys(data1), _.keys(data2));
-  const keysSorted = keys.sort();
-
-  const differences = keysSorted.reduce((acc, key) => {
-    let currentDifferences = acc;
-
-    const addedKey = `\n  + ${key}: ${_.get(data2, key)}`;
-    const oldChangedKey = `\n  - ${key}: ${_.get(data1, key)}`;
-    const newChangedKey = `\n  + ${key}: ${_.get(data2, key)}`;
-    const deletedKey = `\n  - ${key}: ${_.get(data1, key)}`;
-    const unchangedKey = `\n    ${key}: ${_.get(data1, key)}`;
-
-    if (!_.has(data1, key)) {
-      currentDifferences += addedKey;
-    } else if (!_.has(data2, key)) {
-      currentDifferences += deletedKey;
-    } else if (data1[key] !== data2[key]) {
-      currentDifferences += oldChangedKey;
-      currentDifferences += newChangedKey;
-    } else {
-      currentDifferences += unchangedKey;
-    }
-    return currentDifferences;
-  }, '');
-  return `{${differences}\n}`;
+  const data1 = parsers(filepath1);
+  const data2 = parsers(filepath2);
+  const syntaxTree = getSyntaxTree(data1, data2);
+  return getStylishFormat(syntaxTree);
 };
 
 export default getDiff;
