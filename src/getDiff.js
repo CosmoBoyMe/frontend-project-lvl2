f575b45555bea4179959e3ede51efe926a8d2352
CosmoBoyMe <2014failz2014@gmail.com>
2021-01-21 15:10:18 +0100

    fix naming

--- src/getDiff.js
+++ src/getDiff.js
@@ -2,32 +2,35 @@ import _ from 'lodash';
 import parsers from './parsers.js';
 import getFormat from './formatters/index.js';
 
-const getSyntaxTree = (data1, data2) => {
+const makeSyntaxTree = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
 
   const keysSorted = keys.sort();
-  const ast = keysSorted.map((key) => {
+  const syntaxTree = keysSorted.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
     if (!_.has(data1, key)) {
-      return { name: key, value: data2[key], status: 'added' };
+      return { name: key, value: value2, status: 'added' };
     } if (!_.has(data2, key)) {
-      return { name: key, value: data1[key], status: 'removed' };
-    } if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      const children = getSyntaxTree(data1[key], data2[key]);
+      return { name: key, value: value1, status: 'removed' };
+    } if (_.isObject(value1) && _.isObject(value2)) {
+      const children = makeSyntaxTree(value1, value2);
       return { name: key, status: 'nested', children };
-    } if (data1[key] !== data2[key]) {
+    } if (value1 !== value2) {
       return {
-        name: key, value: data2[key], oldValue: data1[key], status: 'updated',
+        name: key, value: value2, oldValue: value1, status: 'updated',
       };
     }
-    return { name: key, value: data1[key], status: 'unchanged' };
+    return { name: key, value: value1, status: 'unchanged' };
   });
-  return ast;
+  return syntaxTree;
 };
 
 const getDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parsers(filepath1);
   const data2 = parsers(filepath2);
-  const syntaxTree = getSyntaxTree(data1, data2);
+  const syntaxTree = makeSyntaxTree(data1, data2);
   const format = getFormat(formatName);
   const diff = format(syntaxTree);
   return diff;
