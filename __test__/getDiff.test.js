18522d337936e187a7768e63617971d35baf3515
CosmoBoyMe <2014failz2014@gmail.com>
2021-01-21 13:46:31 +0100

    add json test

--- __test__/getDiff.test.js
+++ __test__/getDiff.test.js
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import getDiff from '../src/getDiff.js';
 import parsers from '../src/parsers';
+import { expect, test } from '@jest/globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +19,11 @@ test('plainFormat', () => {
   const plainDifference = getDiff(getFixturePath('file1.json'), getFixturePath('file2.yaml'), 'plain');
   const expectedPlain = readFile('expectedPlainFormat.txt');
   expect(plainDifference).toBe(expectedPlain);
+});
+test('jsonFormat', () => {
+  const jsonDifference = getDiff(getFixturePath('file1.json'), getFixturePath('file2.yaml'), 'json');
+  const expectedJson = readFile('expectedJsonFormat.json');
+  expect(jsonDifference).toBe(expectedJson);
 });
 test('parsers', () => {
   const parsingYaml = parsers(getFixturePath('file1.yaml'));
